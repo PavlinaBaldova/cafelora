ff2522a6135dc611f4cfc61c10707f9a60ac271e
PavlinaBaldova <pavlina.bal@seznam.cz>
2020-11-01 14:07:27 +0100

    order drinks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './style.css';
 
 console.log('funguju!');
 
+/*---------navigace-----------*/
+
 const navigation = () => {
   const navBtn = document.querySelector('nav');
   navBtn.classList.toggle('nav-closed');
@@ -20,3 +22,22 @@ const a = document.querySelectorAll('a');
 for (let i = 0; i < a.length; i += 1) {
   a[i].addEventListener('click', aClosed);
 }
+
+/*-------------objednávání-----------*/
+
+let ordered = false;
+
+const drinkCup = () => {
+  const drinkCupElm = document.querySelector('.drink__cup');
+  drinkCupElm.classList.toggle('drink__cup--selected');
+  const btnCancel = document.querySelector('.order-btn');
+  ordered = !ordered;
+  if (ordered === true) {
+    btnCancel.textContent = 'Zrušit';
+  } else {
+    btnCancel.textContent = 'Objednat';
+  }
+};
+
+const orderButton = document.querySelector('.order-btn');
+orderButton.addEventListener('click', drinkCup);
